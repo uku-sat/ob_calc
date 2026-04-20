6fe8258384fe86ee8fcc58c54edf15352074dbfa
kono.t <task4guit@gmail.com>
2026-04-20 12:59:35 +0900

    英語対応のためi18nとidを追加 タワーの名称を数字からαβへ変更

---
+++ i18n.js
@@ -0,0 +1,87 @@
+export const i18n = {
+    ja: {
+        // セクション見出し
+    sectionInput:    '見積入力',
+    sectionMaterial: '使用資材',
+    sectionResult:   '計算結果',
+
+    // 入力ラベル
+    towerHeader1:  'タワー α',
+    towerheader2:  'タワー β',
+    heightLabel:  '見積高さ（cm）',
+    timeLabel:    '提出残り時間（分）',
+
+    // 結果ラベル
+    reward:    '見積報酬',
+    multiplier: '倍率',
+    sales:     '見積売上',
+    purchase:  '購入額',
+    profit:    '利益',
+
+    // ボタン
+    reset: 'リセット',
+
+    // 資材名（data.jsのidと対応させる）
+    materials: {
+      rectangle:       '長方形',
+      rectangleHollow: '長方形のまる抜き',
+      squarePillar:    '角柱',
+      roundPillar:     '丸柱',
+      largeTri:        '大きな三角',
+      smallTri:        '小さな三角',
+      square:          '正方形',
+      circle:          '円形',
+      halfCircle:      '半円',
+    },
+
+     // エラーメッセージ
+    errorHeight1: '高さ1は10〜55の範囲で入力してください',
+    errorHeight2: '高さ2は10〜55の範囲で入力してください',
+    errorTime:    '時間は0〜15の範囲で入力してください',
+
+    // 個数ラベル
+    pieceLabel: '個目',
+    },   
+    en: {
+        // セクション見出し
+    sectionInput:    'Estimate',
+    sectionMaterial: 'Purchase',
+    sectionResult:   'Result',
+
+    // 入力ラベル
+    towerHeader:  'Tower',
+    heightLabel:  'Estimated Height（cm）',
+    timeLabel:    'Time Remaining（min）',
+
+    // 結果ラベル
+    reward:    'Base Reward',
+    multiplier: 'Multiplier',
+    sales:     'Estimated Sales',
+    purchase:  'Purchase Cost',
+    profit:    'Gross Profit',
+
+    // ボタン
+    reset: 'Reset',
+
+    // 資材名（data.jsのidと対応させる）
+    materials: {
+      rectangle:       'Rectangle',
+      rectangleHollow: 'Rectangle with a Cut-out',
+      squarePillar:    'Prism',
+      roundPillar:     'Cylinder',
+      largeTri:        'Large Triangle',
+      smallTri:        'Smal Triangle',
+      square:          'Square',
+      circle:          'Circle',
+      halfCircle:      'Semicircle',
+    },
+
+     // エラーメッセージ
+    errorHeight1: '高さ1は10〜55の範囲で入力してください',
+    errorHeight2: '高さ2は10〜55の範囲で入力してください',
+    errorTime:    '時間は0〜15の範囲で入力してください',
+
+    // 個数ラベル
+    pieceLabel: '個目',
+    }
+}
